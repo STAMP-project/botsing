61fd7f247404f650f66fd27cae462e0ebe69ca2f
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2018-09-08 18:10:34 +0200

    Add single objective genetic algorithm process

---
+++ botsing-reproduction/src/main/java/eu/stamp/botsing/ga/strategy/SingleObjectiveGGA.java
@@ -0,0 +1,352 @@
+package eu.stamp.botsing.ga.strategy;
+
+import com.oracle.tools.packager.Log;
+import eu.stamp.botsing.CrashProperties;
+import eu.stamp.botsing.fitnessfunction.WeightedSum;
+import org.evosuite.Properties;
+import org.evosuite.ga.*;
+import org.evosuite.ga.metaheuristics.GeneticAlgorithm;
+import org.evosuite.ga.metaheuristics.MonotonicGA;
+import org.evosuite.ga.stoppingconditions.MaxFitnessEvaluationsStoppingCondition;
+import org.evosuite.ga.stoppingconditions.StoppingCondition;
+import org.evosuite.testcase.TestCase;
+import org.evosuite.testcase.TestChromosome;
+import org.evosuite.testcase.statements.ConstructorStatement;
+import org.evosuite.testcase.statements.MethodStatement;
+import org.evosuite.testcase.statements.Statement;
+import org.evosuite.utils.LoggingUtils;
+import org.evosuite.utils.Randomness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class SingleObjectiveGGA  <T extends Chromosome> extends GeneticAlgorithm<T> {
+    private static final Logger LOG = LoggerFactory.getLogger(SingleObjectiveGGA.class);
+
+    public SingleObjectiveGGA(ChromosomeFactory factory) {
+        super(factory);
+    }
+    protected ReplacementFunction replacementFunction = new FitnessReplacementFunction();
+    public void setFitnessFunction(){
+
+    }
+
+
+    @Override
+    public void generateSolution() {
+        // generate solution
+        if (population.isEmpty()) {
+            initializePopulation();
+            assert!population.isEmpty() : "Could not create any test";
+        }
+
+        LOG.debug("Starting evolution");
+        int starvationCounter = 0;
+        double bestFitness = Double.MAX_VALUE;
+        double lastBestFitness = Double.MAX_VALUE;
+
+        double bestFFinInitialization = getBestFitness();
+        LOG.debug("Best FF in the initial population is: "+bestFFinInitialization);
+        while (!isFinished()){
+            double bestFitnessBeforeEvolution = getBestFitness();
+            evolve();
+            sortPopulation();
+            double newFitness = getBestFitness();
+            LOG.info("New fitness Function is: "+newFitness);
+
+            if (Double.compare(bestFitness, lastBestFitness) == 0) {
+                starvationCounter++;
+            } else {
+                LOG.info("reset starvationCounter after " + starvationCounter + " iterations");
+                starvationCounter = 0;
+                lastBestFitness = bestFitness;
+
+            }
+
+            updateSecondaryCriterion(starvationCounter);
+
+            LOG.info("Current iteration: " + currentIteration);
+            this.notifyIteration();
+
+        }
+    }
+
+    @Override
+    protected void evolve() {
+        List<T> newGeneration = new ArrayList<T>();
+        // Elitism
+        LOG.debug("Selection");
+
+            newGeneration.addAll(elitism());
+
+        while (!isPopulationFull(newGeneration) && !isFinished()) {
+            LOG.debug("Generating offspring");
+            T parent1 = selectionFunction.select(population);
+            T parent2 = selectionFunction.select(population);
+            T offspring1 = (T) parent1.clone();
+            T offspring2 = (T) parent2.clone();
+            // Crossover
+            if (Randomness.nextDouble() <= Properties.CROSSOVER_RATE) {
+                try{
+                    crossoverFunction.crossOver(offspring1, offspring2);
+                }catch (ConstructionFailedException e){
+					LOG.error("construction failed when doing crossover!");
+                    continue;
+                }catch (Exception e) {
+					LOG.error("Exception during the crossover!");
+                }
+
+            }
+            // Check if offsprings contain the target method call
+            if (!includesPublicCall(offspring1)) {
+                offspring1 = (T) parent1.clone();
+            } else if(!includesPublicCall(offspring2)) {
+                offspring2 = (T) parent2.clone();
+            }
+
+            // Mutation
+            try {
+                mutateOffspring(offspring1);
+                mutateOffspring(offspring2);
+            }catch (Exception e) {
+                LOG.error("Mutation is unsuccessful");
+                e.printStackTrace();
+            }
+            // If and only if one of the offsprings is not worse than the best parent, we replace parents by offsprings.
+            FitnessFunction fitnessFunction = fitnessFunctions.get(0);
+            fitnessFunction.getFitness(offspring1);
+            notifyEvaluation(offspring1);
+            fitnessFunction.getFitness(offspring2);
+            notifyEvaluation(offspring2);
+
+            if (keepOffspring(parent1, parent2, offspring1, offspring2)) {
+                LOG.debug("Replace parents");
+
+                // Reject offspring straight away if it's too long
+                int rejected = 0;
+                if (isTooLong(offspring1) || offspring1.size() == 0) {
+                    rejected++;
+                } else {
+                    // if(Properties.ADAPTIVE_LOCAL_SEARCH ==
+                    // AdaptiveLocalSearchTarget.ALL)
+                    // applyAdaptiveLocalSearch(offspring1);
+                    newGeneration.add(offspring1);
+                }
+
+                if (isTooLong(offspring2) || offspring2.size() == 0) {
+                    rejected++;
+                } else {
+                    // if(Properties.ADAPTIVE_LOCAL_SEARCH ==
+                    // AdaptiveLocalSearchTarget.ALL)
+                    // applyAdaptiveLocalSearch(offspring2);
+                    newGeneration.add(offspring2);
+                }
+
+                if (rejected == 1)
+                    newGeneration.add(Randomness.choice(parent1, parent2));
+                else if (rejected == 2) {
+                    newGeneration.add(parent1);
+                    newGeneration.add(parent2);
+                }
+            } else {
+                LOG.debug("Keep parents");
+                newGeneration.add(parent1);
+                newGeneration.add(parent2);
+            }
+        }
+
+
+        population = newGeneration;
+        // archive
+        updateFitnessFunctionsAndValues();
+
+        currentIteration++;
+
+    }
+
+    private boolean isPopulationFull(List<T> newGeneration) {
+        try {
+            if(newGeneration.size() >= CrashProperties.getIntValue("population")) {
+                return true;
+            }
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (Properties.NoSuchParameterException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    protected List<T>  elitism() {
+        List<T> elite = new ArrayList<T>();
+        LOG.debug("Cloning the best individuals to next generation");
+        try {
+            for (int i = 0; i < CrashProperties.getIntValue("elite"); i++) {
+                elite.add(population.get(i));
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return elite;
+    }
+
+    @Override
+    public void initializePopulation() {
+        currentIteration = 0;
+        // Generate Initial Population
+        try {
+            generatePopulation(CrashProperties.getIntValue("population"));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (Properties.NoSuchParameterException e) {
+            e.printStackTrace();
+        }
+
+        LOG.debug("Initializing the population.");
+        // Calculate fitness functions
+        calculateFitnessOfPopulation();
+        // Sort individuals
+        sortPopulation();
+    }
+
+    protected void sortPopulation() {
+        LOG.debug("Sort current population.");
+
+
+        if (fitnessFunctions.get(0).isMaximizationFunction()) {
+            Collections.sort(population, Collections.reverseOrder());
+        } else {
+            Collections.sort(population);
+        }
+    }
+
+    private void calculateFitnessOfPopulation() {
+        LOG.debug("Calculating fitness for " + population.size() + " individuals");
+        Iterator<T> iterator = population.iterator();
+        while (iterator.hasNext()) {
+            T c = iterator.next();
+            if (isFinished()) {
+                if (c.isChanged())
+                    iterator.remove();
+            } else {
+                for (FitnessFunction<T> fitnessFunction : fitnessFunctions) {
+                    notifyEvaluation(c);
+                    fitnessFunction.getFitness(c);
+                }
+            }
+        }
+    }
+
+
+
+    private void generatePopulation(int populationSize) {
+        LOG.debug("Creating random population");
+        for (int i = 0; i < populationSize; i++) {
+            T individual;
+            individual = chromosomeFactory.getChromosome();
+            for (FitnessFunction<?> fitnessFunction : this.fitnessFunctions) {
+                individual.addFitness(fitnessFunction);
+            }
+
+            population.add(individual);
+
+            if (isFinished())
+                break;
+        }
+    }
+
+
+    public boolean isFinished() {
+        for (StoppingCondition c : stoppingConditions) {
+            LOG.debug("Current value of stopping condition "+ c.getClass().toString()+": "+c.getCurrentValue());
+
+            // logger.error(c + " "+ c.getCurrentValue());
+            if (c.isFinished()){
+                LoggingUtils.getEvoLogger().info(c.toString());
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private double getBestFitness() {
+        T bestIndividual = getBestIndividual();
+        for (FitnessFunction<T> ff : fitnessFunctions) {
+            ff.getFitness(bestIndividual);
+        }
+        return bestIndividual.getFitness();
+    }
+
+    public T getBestIndividual() {
+
+        if (population.isEmpty()) {
+            return this.chromosomeFactory.getChromosome();
+        }
+
+        // Assume population is sorted
+        return population.get(0);
+    }
+
+    private boolean includesPublicCall (T individual) {
+
+        Iterator <String> publicCallsIterator = WeightedSum.publicCalls.iterator();
+        TestChromosome candidateChrom = (TestChromosome) individual;
+        TestCase candidate = candidateChrom.getTestCase();
+        if (candidate.size() == 0){
+            return true;
+        }
+        while (publicCallsIterator.hasNext()){
+            String callName = publicCallsIterator.next();
+            for ( int index= 0 ; index < candidate.size() ;index++) {
+                Statement currentStatement = candidate.getStatement(index);
+                if (currentStatement instanceof MethodStatement) {
+                    MethodStatement candidateMethod = (MethodStatement) candidate.getStatement(index);
+                    if (candidateMethod.getMethodName().equalsIgnoreCase(callName)) {
+                        return true;
+                    }
+                } else if (currentStatement instanceof ConstructorStatement && callName.contains(".")){
+//                    ConstructorStatement candidateConstructor = (ConstructorStatement) candidate.getStatement(index);
+//                    if(callName.contains(".")){
+                        return true;
+//                    }
+                }else {
+
+                    //FIXME: We should detec all of the target methods
+//					LOG.error("The method is neither a constructor nor method call!");
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private void mutateOffspring (T offspring) {
+        boolean permission = false;
+        while (!permission) {
+            // Mutation
+            try{
+                offspring.mutate();
+            }catch(Exception e){
+                LOG.error("Mutation failed!");
+            }
+            if (offspring.isChanged()) {
+                offspring.updateAge(currentIteration);
+            }
+            try {
+                permission = includesPublicCall(offspring);
+            }catch(Exception e){
+                LOG.error("* EvoCrash: Something went wrong when checking the target call after mutation! \n ");
+            }
+        }
+    }
+
+    protected boolean keepOffspring(Chromosome parent1, Chromosome parent2, Chromosome offspring1,
+                                    Chromosome offspring2) {
+        return replacementFunction.keepOffspring(parent1, parent2, offspring1, offspring2);
+    }
+}
---
+++ botsing-reproduction/src/test/java/eu/stamp/botsing/SimpleCrashReproductionTest.java
@@ -0,0 +1,54 @@
+package eu.stamp.botsing;
+
+import ch.qos.logback.classic.Level;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Ignore
+public class SimpleCrashReproductionTest {
+
+    public static int target_frame_level = 2;
+
+    public static String user_dir = System.getProperty("user.dir");
+    public static String test_dir = Paths.get(user_dir,"src","test","java","eu","stamp","botsing").toString();
+    public static String log_dir = Paths.get(test_dir, "sample.log").toString();
+    public static String  bin_path = Paths.get(test_dir, "sample_dep").toString();
+    private static String separator = System.getProperty("path.separator");
+
+    @Test
+    public void runtest(){
+        setLoggingLevel(Level.INFO);
+
+        BotSing botsing = new BotSing();
+        String[] prop = {
+                "-Dcrash_log="+log_dir,
+                "-Dtarget_frame="+target_frame_level,
+                "-projectCP",
+                getListOfDeps(),
+        };
+        botsing.parseCommandLine(prop);
+    }
+
+    public static void setLoggingLevel(ch.qos.logback.classic.Level level) {
+        ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+        root.setLevel(level);
+    }
+
+    public static String getListOfDeps(){
+        String dependencies = "";
+        File depFolder = new File(bin_path);
+        File[] listOfFilesInSourceFolder = depFolder.listFiles();
+        for(int i = 0; i < listOfFilesInSourceFolder.length; i++) {
+            if (listOfFilesInSourceFolder[i].getName().charAt(0) != '.') {
+                Path depPath = Paths.get(depFolder.getAbsolutePath(), listOfFilesInSourceFolder[i].getName());
+                dependencies += (depPath.toString() + separator);
+            }
+        }
+        return dependencies;
+    }
+
+}
