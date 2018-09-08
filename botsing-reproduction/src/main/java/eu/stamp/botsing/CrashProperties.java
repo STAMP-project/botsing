9c8ec40fab13c8da499172ea4ef89c516863e838
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2018-09-08 18:07:54 +0200

    Update Crash properties

--- botsing-reproduction/src/main/java/eu/stamp/botsing/CrashProperties.java
+++ botsing-reproduction/src/main/java/eu/stamp/botsing/CrashProperties.java
@@ -4,9 +4,14 @@ package eu.stamp.botsing;
 import org.apache.commons.cli.CommandLine;
 import org.evosuite.Properties;
 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.nio.file.Paths;
 
 
@@ -15,16 +20,41 @@ public class CrashProperties {
     private static CrashProperties instance = null;
     private StackTrace crash = StackTrace.getInstance();
     private String[] projectClassPaths;
-    public static enum CrashReproductionSearchStrategy {
-        Simple_GGA;
 
-        private CrashReproductionSearchStrategy() {
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.FIELD)
+    public @interface Parameter {
+        String key();
+
+        String group() default "Experimental";
+
+        String description();
+    }
+    public static enum TestGenerationStrategy {
+        Single_GA;
+
+        private TestGenerationStrategy() {
         }
     }
 
-    @Properties.Parameter(key = "CrashReproductionSearchStrategy", group = "Crash reproduction", description = "Which mode to use for crash reproduction")
-    public static CrashProperties.CrashReproductionSearchStrategy STRATEGY = CrashProperties.CrashReproductionSearchStrategy.Simple_GGA;
+    public static enum SearchAlgorithm{
+        Single_Objective_GGA;
+        private SearchAlgorithm(){}
+    }
 
+
+
+    @Properties.Parameter(key = "testGenerationStrategy", group = "Crash reproduction", description = "Which mode to use for crash reproduction")
+    public static CrashProperties.TestGenerationStrategy testGenerationStrategy = CrashProperties.TestGenerationStrategy.Single_GA;
+
+
+    @Properties.Parameter(key = "SearchAlgorithm", group = "Crash reproduction", description = "Which search algorithm to use for crash reproduction")
+    public static CrashProperties.SearchAlgorithm searchAlgorithm = SearchAlgorithm.Single_Objective_GGA;
+
+
+    /** The target frame in the crash stack trace */
+    @Parameter(key = "max_target_injection_tries", group = "Runtime", description = "The maximum number of times the search tries to generate an individuals with the target method.")
+    public static int max_target_injection_tries = 150;
 
     static java.util.Properties configFile = new java.util.Properties();
     private CrashProperties(){
@@ -60,13 +90,24 @@ public class CrashProperties {
         return instance;
     }
 
-    public String getStringValue(String property) throws IllegalAccessException, Properties.NoSuchParameterException {
+    public static String getStringValue(String property) throws IllegalAccessException, Properties.NoSuchParameterException {
         if (Properties.hasParameter(property)){
             return Properties.getStringValue(property);
         }else if (configFile.containsKey(property)){
             return configFile.getProperty(property);
         }
         return null;
+    }
+
+
+
+    public static int getIntValue(String property) throws IllegalAccessException, Properties.NoSuchParameterException {
+            return Properties.getIntegerValue(property);
+    }
+
+
+    public static long getLongValue(String property) throws IllegalAccessException, Properties.NoSuchParameterException {
+        return Properties.getLongValue(property);
     }
 
 
@@ -103,10 +144,19 @@ public class CrashProperties {
         return crash;
     }
 
-    public Properties.StoppingCondition getStoppingCondition(){
+    public static Properties.StoppingCondition getStoppingCondition(){
         return Properties.STOPPING_CONDITION;
+    }
+    public static Throwable getTargetException () {
+        StackTraceElement [] stackArray = new StackTraceElement [StackTrace.getInstance().getNumberOfFrames()];
+        stackArray = StackTrace.getInstance().getFrames().toArray(stackArray);
+        Throwable targetException = new Exception();
+        targetException.setStackTrace(stackArray);
+
+        return targetException;
     }
 
 
 
-}
+
+        }
