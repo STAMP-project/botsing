8e78c878a0f410694a292a84d819f757870cff39
apanichella <a.panichella@tudelft.nl>
2018-10-11 00:54:11 +0200

    Adding test cases

--- botsing-reproduction/src/test/java/eu/stamp/botsing/CommandLineParametersTest.java
+++ botsing-reproduction/src/test/java/eu/stamp/botsing/CommandLineParametersTest.java
@@ -1,5 +1,42 @@
+package eu.stamp.botsing;
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.Iterator;
+
 import static org.junit.Assert.*;
 
 public class CommandLineParametersTest {
 
+    @Test
+    public void testGetCommandLineOptions() {
+        Options opt = CommandLineParameters.getCommandLineOptions();
+        Collection<Option> opts = opt.getOptions();
+        Iterator<Option> iter = opts.iterator();
+
+        Option opt1 = iter.next();
+        assertTrue(opt1.getDescription().equalsIgnoreCase("use value for given property"));
+        assertTrue(opt1.getArgName().equalsIgnoreCase("property=value"));
+        assertTrue(opt1.getOpt().equalsIgnoreCase("D"));
+
+        Option opt2 = iter.next();
+        assertTrue(opt2.getDescription().equalsIgnoreCase("classpath of the project under test and all its dependencies"));
+        assertTrue(opt2.getArgName() == null);
+        assertTrue(opt2.getOpt().equalsIgnoreCase("projectCP"));
+
+        Option opt3 = iter.next();
+        assertTrue(opt3.getDescription().equalsIgnoreCase("Level of the target frame"));
+        assertTrue(opt3.getArgName() == null);
+        assertTrue(opt3.getOpt().equalsIgnoreCase("target_frame"));
+
+        Option opt4 = iter.next();
+        assertTrue(opt4.getDescription().equalsIgnoreCase("Directory of the given stack trace"));
+        assertTrue(opt4.getArgName() == null);
+        assertTrue(opt4.getOpt().equalsIgnoreCase("crash_log"));
+
+        assertFalse(iter.hasNext());
+    }
 }
--- botsing-reproduction/src/test/java/eu/stamp/botsing/CrashPropertiesTest.java
+++ botsing-reproduction/src/test/java/eu/stamp/botsing/CrashPropertiesTest.java
@@ -1,5 +1,53 @@
+package eu.stamp.botsing;
+
+import org.apache.commons.cli.*;
+import org.evosuite.Properties;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class CrashPropertiesTest {
+
+    @Test
+    public void testSetupStackTrace() throws ParseException {
+        CrashProperties properties = CrashProperties.getInstance();
+        //assertNull(properties.getProjectClassPaths());
+
+        StackTrace crash = properties.getStackTrace();
+        assertNull(crash.getExceptionType());
+        assertNull(crash.getFrames());
+        assertNull(crash.getTargetClass());
+    }
+
+    @Test
+    public void testGetStringValue() throws Properties.NoSuchParameterException, IllegalAccessException {
+        String value = CrashProperties.getStringValue("D");
+        System.out.println(value);
+    }
+
+    @Test
+    public void testProperties() throws Properties.NoSuchParameterException, IllegalAccessException {
+        int popsize = CrashProperties.getIntValue("population");
+        assertEquals(100, popsize);
+
+        long budget = CrashProperties.getLongValue("search_budget");
+        assertEquals(1800, budget);
+
+        boolean bool = CrashProperties.getBooleanValue("sandbox");
+        assertEquals(true, bool);
+
+        Properties.StoppingCondition cond = CrashProperties.getStoppingCondition();
+        assertEquals("MAXTIME",cond.toString());
+    }
+
+    @Test
+    public void testSetClasspath() throws Properties.NoSuchParameterException, IllegalAccessException {
+        CrashProperties properties = CrashProperties.getInstance();
+        properties.setClasspath("jar1:jar2");
+        String[] jars = properties.getProjectClassPaths();
+
+        assertEquals("jar1", jars[0]);
+        assertEquals("jar2", jars[1]);
+    }
 
 }
