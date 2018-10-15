55ac06534a5d135fddb83c60997b9bb30f50147a
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2018-10-15 14:50:26 +0200

    Handle unsuccessful random insertion mutation error.

--- botsing-reproduction/src/main/java/eu/stamp/botsing/ga/strategy/operators/GuidedMutation.java
+++ botsing-reproduction/src/main/java/eu/stamp/botsing/ga/strategy/operators/GuidedMutation.java
@@ -16,13 +16,18 @@ public class GuidedMutation<T extends Chromosome> {
         // let's try one single mutation
         offspring.mutate();
 
-        // if the chromosome has no public call, we instert new random statements
+        // if the chromosome has no public call, we insert new random statements
         boolean isValid = utility.includesPublicCall(offspring);
         int nTrials = 0; // we try maximum 50 insertion mutations (to avoid infinite loop)
         while (!isValid && nTrials<50){
-            ((TestChromosome) offspring).mutationInsert();
-            isValid = utility.includesPublicCall(offspring);
-            nTrials++;
+            try {
+                ((TestChromosome) offspring).mutationInsert();
+                isValid = utility.includesPublicCall(offspring);
+                nTrials++;
+            }catch (AssertionError e){
+                LOG.warn("Random insertion mutation was unsuccessful.");
+            }
+
         }
         offspring.updateAge(offspring.getAge()+1);
     }
