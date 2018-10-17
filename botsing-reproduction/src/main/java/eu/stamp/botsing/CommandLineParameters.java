c1ac67cac283e91fae0b73ee2148667db476ac26
Xavier Devroey <xavier.devroey@gmail.com>
2018-10-17 13:18:43 +0200

    Updating option creation to remove call to deprecated method

--- botsing-reproduction/src/main/java/eu/stamp/botsing/CommandLineParameters.java
+++ botsing-reproduction/src/main/java/eu/stamp/botsing/CommandLineParameters.java
@@ -21,7 +21,6 @@ package eu.stamp.botsing;
  */
 
 import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 
 public class CommandLineParameters {
@@ -30,7 +29,12 @@ public class CommandLineParameters {
         Options options = new Options();
         // define options
         @SuppressWarnings("static-access")
-        Option property = OptionBuilder.withArgName("property=value").hasArgs(2).withValueSeparator().withDescription("use value for given property").create("D");
+        Option property = Option.builder("D")
+                .numberOfArgs(2)
+                .argName("property=value")
+                .valueSeparator()
+                .desc("use value for given property")
+                .build();
         Option projectCP = new Option("projectCP", true,
                 "classpath of the project under test and all its dependencies");
         Option target_frame = new Option("target_frame",true, "Level of the target frame");
