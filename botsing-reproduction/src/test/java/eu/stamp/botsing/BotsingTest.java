4bd812d8119f387b6a0015b1c3284f7e92e8b20d
apanichella <a.panichella@tudelft.nl>
2018-10-18 15:11:48 +0200

    Remove unused code

--- botsing-reproduction/src/test/java/eu/stamp/botsing/BotsingTest.java
+++ botsing-reproduction/src/test/java/eu/stamp/botsing/BotsingTest.java
@@ -1,14 +1,10 @@
 package eu.stamp.botsing;
 
-import org.apache.commons.cli.CommandLine;
-import org.evosuite.Properties;
 import org.evosuite.result.TestGenerationResult;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 import java.io.File;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -41,27 +37,5 @@ public class BotsingTest {
         List<TestGenerationResult> results = botsing.parseCommandLine(prop);
         assertTrue(results.size() > 0);
         assertEquals(TestGenerationResult.Status.SUCCESS, results.get(0).getTestGenerationStatus());
-    }
-
-    private StackTraceElement stringToStackTraceElement(String frameString, String filename){
-        int startPoint = frameString.indexOf("at ")+3;
-        String usefulPart = frameString.substring(startPoint);
-        int splitPoint = usefulPart.indexOf("(");
-        String usefulForLineDetection = usefulPart.substring(splitPoint);
-        String usefulForOtherParts = usefulPart.substring(0,splitPoint);
-
-        //Line detection
-        int lineFirstSplitpoint = usefulForLineDetection.indexOf(":")+1;
-        int lineSecondSplitpoint = usefulForLineDetection.indexOf(")");
-
-        int lineNumber = Integer.parseInt(usefulForLineDetection.substring(lineFirstSplitpoint,lineSecondSplitpoint));
-
-        String[] split = usefulForOtherParts.split("\\.");
-        // method Detection
-        String methodName =  split[(split.length-1)];
-
-        // class detection
-        String clazz = String.join(".",Arrays.copyOfRange(split,0,split.length-1));
-        return new StackTraceElement(clazz, methodName, filename, lineNumber);
     }
 }
