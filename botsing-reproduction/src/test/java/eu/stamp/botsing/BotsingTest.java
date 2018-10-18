841a86e925129633655609a289dbc140c31d7cac
apanichella <a.panichella@tudelft.nl>
2018-10-18 15:41:11 +0200

    Add some statement for debugging on the server

--- botsing-reproduction/src/test/java/eu/stamp/botsing/BotsingTest.java
+++ botsing-reproduction/src/test/java/eu/stamp/botsing/BotsingTest.java
@@ -1,5 +1,6 @@
 package eu.stamp.botsing;
 
+import ch.qos.logback.classic.Level;
 import org.evosuite.result.TestGenerationResult;
 import org.junit.Test;
 
@@ -18,12 +19,24 @@ public class BotsingTest {
 
     @Test
     public void testFractionCrash() {
+        ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+        root.setLevel(Level.INFO);
+
         Botsing botsing = new Botsing();
 
         // setup of the directory with file *class and file *.log
         String user_dir = System.getProperty("user.dir"); // the current directory is the module <b>botsing-reproduction</b>
         File file = new File(user_dir);
         String base_dir = Paths.get(file.getParent(), "botsing-examples").toString(); // the crash to replicate is inside the module <b>botsing-examples</b>
+
+        File f = new File(base_dir);
+        assertTrue(f.exists());
+
+        f = new File(Paths.get(base_dir, "src","main","resources","Fraction.log").toString());
+        System.out.println(f.getAbsoluteFile()+ "Exists? "+f.exists());
+
+        f = new File(Paths.get(base_dir, "target","classes").toString());
+        System.out.println(f.getAbsoluteFile()+ "Exists? "+f.exists());
 
         //run Botsing
         String[] prop = {
