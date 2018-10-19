27fa842166fabc40792f921a26871bbfa502e855
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2018-10-19 16:44:11 +0200

    Initialize botsing-model-generation

---
+++ botsing-model-generation/src/main/java/eu/stamp/botsing_model_generation/CommandLineParameters.java
@@ -0,0 +1,46 @@
+package eu.stamp.botsing_model_generation;
+
+/*-
+ * #%L
+ * botsing-reproduction
+ * %%
+ * Copyright (C) 2017 - 2018 eu.stamp-project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
+public class CommandLineParameters {
+
+    public static final String PROJECT_CP_OPT = "projectCP";
+    public static final String HELP_OPT = "help";
+
+    public static Options getCommandLineOptions() {
+        Options options = new Options();
+        // Classpath
+        options.addOption(Option.builder(PROJECT_CP_OPT)
+                .hasArg()
+                .desc("classpath of the project under test and all its dependencies")
+                .build());
+        // Help message
+        options.addOption(Option.builder(HELP_OPT)
+                .desc("Prints this help message.")
+                .build());
+
+        return options;
+    }
+
+}
---
+++ botsing-model-generation/src/main/java/eu/stamp/botsing_model_generation/Main.java
@@ -0,0 +1,72 @@
+package eu.stamp.botsing_model_generation;
+
+import eu.stamp.botsing_model_generation.generation.behavioral_model.ModelGeneration;
+import eu.stamp.botsing_model_generation.generation.behavioral_model.model.Model;
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import java.io.File;
+
+import static eu.stamp.botsing_model_generation.CommandLineParameters.*;
+
+public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        Main modelGenerator = new Main();
+        modelGenerator.parseCommandLine(args);
+
+
+    }
+
+
+    public  void parseCommandLine(String[] args) {
+        Options options = CommandLineParameters.getCommandLineOptions();
+        CommandLine commands = parseCommands(args, options);
+        if (commands.hasOption(HELP_OPT)){
+            printHelpMessage(options);
+        }else if(commands.hasOption(PROJECT_CP_OPT)){ // Generate the model for passed cps
+            ModelGeneration modelGeneration;
+            File file = new File(commands.getOptionValue(PROJECT_CP_OPT));
+            if(file.isDirectory()) {
+                File[] jarsFiles = file.listFiles((File f) -> f.isFile() && f.getName().endsWith(".jar"));
+                String[] jarsCp = new String[jarsFiles.length];
+                for (int i = 0; i < jarsCp.length; i++) {
+                    jarsCp[i] = jarsFiles[i].getAbsolutePath();
+                }
+                modelGeneration =  new ModelGeneration(jarsCp);
+            }else{
+                modelGeneration =  new ModelGeneration(commands.getOptionValue(PROJECT_CP_OPT));
+            }
+            Model result = modelGeneration.generate();
+            if(result == null){
+                LOG.error("The generated model is NULL!");
+            }
+
+        }else{
+            LOG.error("Project classpath should be passed as an input. For more information -> help");
+        }
+
+    }
+
+
+    protected CommandLine parseCommands(String[] args, Options options){
+        CommandLineParser parser = new DefaultParser();
+        CommandLine commands = null;
+        try {
+            commands = parser.parse(options, args);
+        } catch (ParseException e) {
+            LOG.error("Could not parse command line!", e);
+            printHelpMessage(options);
+            return null;
+        }
+        return commands;
+    }
+
+    private void printHelpMessage(Options options) {
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("java -jar botsing_model_generator.jar -projectCP dep1.jar;dep2.jar  )", options);
+    }
+}
