7ceb767d2a246d62c01d02fc4bd49ac6695de957
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2018-11-04 19:48:18 +0100

    Refactor CaptureUtil

--- botsing-model-generation/src/main/java/eu/stamp/botsing_model_generation/testcase/carving/capture/CaptureUtil.java
+++ botsing-model-generation/src/main/java/eu/stamp/botsing_model_generation/testcase/carving/capture/CaptureUtil.java
@@ -20,30 +20,7 @@ public final class CaptureUtil {
     }
 
     public static final Class<?> getClassFromDesc(final String desc) {
-        final Type type = Type.getType(desc);
-        if(type.equals(Type.BOOLEAN_TYPE)) {
-            return boolean.class;
-        }else if(type.equals(Type.BYTE_TYPE)){
-            return byte.class;
-        }else if(type.equals(Type.CHAR_TYPE)){
-            return char.class;
-        }else if(type.equals(Type.DOUBLE_TYPE)){
-            return double.class;
-        }else if(type.equals(Type.FLOAT_TYPE)){
-            return float.class;
-        }else if(type.equals(Type.INT_TYPE)){
-            return int.class;
-        }else if(type.equals(Type.LONG_TYPE)){
-            return long.class;
-        }else if(type.equals(Type.SHORT_TYPE)){
-            return short.class;
-        }
-
-        try{
-            return Class.forName(ResourceList.getClassNameFromResourcePath(type.getInternalName()));
-        }catch (final ClassNotFoundException e){
-            throw new RuntimeException(e);
-        }
+        return org.evosuite.testcarver.capture.CaptureUtil.getClassFromDesc(desc);
     }
 
 
