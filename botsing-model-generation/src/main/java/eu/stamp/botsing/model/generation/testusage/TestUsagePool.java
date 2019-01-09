79521d575d15a9b1fb345ab780f9fb51515f4cbd
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2019-01-09 13:39:57 +0100

    Add test usage pool for collecting the accessed objects by the test cases

---
+++ botsing-model-generation/src/main/java/eu/stamp/botsing/model/generation/testusage/TestUsagePool.java
@@ -0,0 +1,23 @@
+package eu.stamp.botsing.model.generation.testusage;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+
+public class TestUsagePool {
+    private static final Logger LOG = LoggerFactory.getLogger(TestUsagePool.class);
+    protected Map<String, List<String>> pool = new HashMap<String, List<String>>();
+
+    public void addTest(String usedClass,String testName){
+        LOG.info("Adding test {} to class {}.",testName,usedClass);
+        if(!this.pool.containsKey(usedClass)){
+            this.pool.put(usedClass,new ArrayList<>());
+        }
+
+        if(!this.pool.get(usedClass).contains(testName)){
+            this.pool.get(usedClass).add(testName);
+        }
+    }
+}
