636be067499e68a9bf137a17ba93f6d357a1af52
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2019-01-22 16:45:23 +0100

    Update access level of methods is GuidedGeneticAlgorithm

--- botsing-reproduction/src/main/java/eu/stamp/botsing/ga/strategy/GuidedGeneticAlgorithm.java
+++ botsing-reproduction/src/main/java/eu/stamp/botsing/ga/strategy/GuidedGeneticAlgorithm.java
@@ -204,7 +204,7 @@ public class GuidedGeneticAlgorithm<T extends Chromosome> extends GeneticAlgorit
     protected List<T>  elitism() {
         List<T> elite = new ArrayList<T>();
         LOG.debug("Cloning the best individuals to next generation");
-        for (int i = 0; i < this.eliteSize; i++) {
+        for (int i = 0; i < eliteSize; i++) {
             elite.add(population.get(i));
         }
         return elite;
@@ -236,7 +236,7 @@ public class GuidedGeneticAlgorithm<T extends Chromosome> extends GeneticAlgorit
         }
     }
 
-    private void calculateFitness() {
+    protected void calculateFitness() {
         LOG.debug("Calculating fitness for " + population.size() + " individuals");
         Iterator<T> iterator = population.iterator();
         while (iterator.hasNext()) {
@@ -251,7 +251,7 @@ public class GuidedGeneticAlgorithm<T extends Chromosome> extends GeneticAlgorit
         }
     }
 
-    private void calculateFitness(T chromosome){
+    protected void calculateFitness(T chromosome){
         for (FitnessFunction<T> fitnessFunction : fitnessFunctions) {
             notifyEvaluation(chromosome);
             double value = fitnessFunction.getFitness(chromosome);
