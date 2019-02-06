e1b2a5bb1fd58a228a36797e1e4558d0d1fe90b5
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2019-02-06 13:44:41 +0100

    Define ClassInstrumentation in botsing commons

---
+++ botsing-commons/src/main/java/eu/stamp/botsing/commons/instrumentation/ClassInstrumentation.java
@@ -0,0 +1,26 @@
+package eu.stamp.botsing.commons.instrumentation;
+
+import eu.stamp.botsing.commons.BotsingTestGenerationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassInstrumentation {
+    private static final Logger LOG = LoggerFactory.getLogger(ClassInstrumentation.class);
+    public List<Class> instrumentClasses(List<String> interestingClasses){
+        List<Class> instrumentedClasses = new ArrayList<>();
+        for(String clazz: interestingClasses){
+            LOG.info("Instrumenting class "+ clazz);
+            Class<?> cls;
+            try {
+                cls = Class.forName(clazz,false, BotsingTestGenerationContext.getInstance().getClassLoaderForSUT());
+                instrumentedClasses.add(cls);
+            } catch (ClassNotFoundException | NoClassDefFoundError e) {
+                LOG.warn("Error in loading {}",clazz);
+            }
+        }
+        return instrumentedClasses;
+    }
+}
