708560ac139eebe6fdcfad03a092358d381af016
Xavier Devroey <xavier.devroey@gmail.com>
2019-03-01 17:44:06 +0100

    Fixing failing test

--- botsing-reproduction/src/test/java/eu/stamp/botsing/BotsingTest.java
+++ botsing-reproduction/src/test/java/eu/stamp/botsing/BotsingTest.java
@@ -3,7 +3,6 @@ package eu.stamp.botsing;
 import ch.qos.logback.classic.Level;
 import org.evosuite.Properties;
 import org.evosuite.result.TestGenerationResult;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -17,12 +16,13 @@ import java.io.File;
 import java.nio.file.Paths;
 import java.util.List;
 
-import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.io.FileMatchers.*;
+import static org.hamcrest.io.FileMatchers.anExistingDirectory;
+import static org.junit.Assert.*;
 
 /**
- * This class contains integration test cases for botsing. Each test method runs Botsing against one of the exmaple of crashes in the module <b>botsing-example</b>
+ * This class contains integration test cases for botsing. Each test method runs Botsing against one of the exmaple of
+ * crashes in the module <b>botsing-example</b>
  */
 public class BotsingTest {
 
@@ -42,7 +42,8 @@ public class BotsingTest {
     @Before
     public void initialize() {
         Properties.RANDOM_SEED = (long) 1;
-        ch.qos.logback.classic.Logger root = (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+        ch.qos.logback.classic.Logger root =
+                (ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory.getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
         root.setLevel(Level.WARN);
     }
 
@@ -51,9 +52,11 @@ public class BotsingTest {
         Botsing botsing = new Botsing();
 
         // setup of the directory with file *class and file *.log
-        String user_dir = System.getProperty("user.dir"); // the current directory is the module <b>botsing-reproduction</b>
+        String user_dir = System.getProperty("user.dir"); // the current directory is the module
+        // <b>botsing-reproduction</b>
         File file = new File(user_dir);
-        String base_dir = Paths.get(file.getParent(), "botsing-examples").toString(); // the crash to replicate is inside the module <b>botsing-examples</b>
+        String base_dir = Paths.get(file.getParent(), "botsing-examples").toString(); // the crash to replicate is
+        // inside the module <b>botsing-examples</b>
 
         // Set the output directory
         File outputDir = Paths.get(user_dir, "target", "crash-reproduction-tests").toFile();
@@ -85,9 +88,11 @@ public class BotsingTest {
         Botsing botsing = new Botsing();
 
         // setup of the directory with file *class and file *.log
-        String user_dir = System.getProperty("user.dir"); // the current directory is the module <b>botsing-reproduction</b>
+        String user_dir = System.getProperty("user.dir"); // the current directory is the module
+        // <b>botsing-reproduction</b>
         File file = new File(user_dir);
-        String base_dir = Paths.get(file.getParent(), "botsing-examples").toString(); // the crash to replicate is inside the module <b>botsing-examples</b>
+        String base_dir = Paths.get(file.getParent(), "botsing-examples").toString(); // the crash to replicate is
+        // inside the module <b>botsing-examples</b>
 
         // Set the output directory
         File outputDir = Paths.get(user_dir, "target", "crash-reproduction-tests").toFile();
@@ -115,8 +120,10 @@ public class BotsingTest {
 
     @Test
     public void testHelpOption() {
+        Botsing botsing = new Botsing();
         String[] prop = {"-" + CommandLineParameters.HELP_OPT};
-        Botsing.main(prop);
+        Object result = botsing.parseCommandLine(prop);
+        assertThat("Call with option " + CommandLineParameters.HELP_OPT, result, nullValue());
     }
 
     @Test
@@ -129,7 +136,7 @@ public class BotsingTest {
                 "path"
         };
         Object result = botsing.parseCommandLine(prop);
-        assertThat("Missing option "+CommandLineParameters.CRASH_LOG_OPT, result, nullValue());
+        assertThat("Missing option " + CommandLineParameters.CRASH_LOG_OPT, result, nullValue());
 
         botsing = new Botsing();
         prop = new String[]{
@@ -139,7 +146,7 @@ public class BotsingTest {
                 "path"
         };
         result = botsing.parseCommandLine(prop);
-        assertThat("Missing option "+CommandLineParameters.TARGET_FRAME_OPT, result, nullValue());
+        assertThat("Missing option " + CommandLineParameters.TARGET_FRAME_OPT, result, nullValue());
 
         botsing = new Botsing();
         prop = new String[]{
@@ -150,7 +157,7 @@ public class BotsingTest {
         };
 
         result = botsing.parseCommandLine(prop);
-        assertThat("Missing option "+CommandLineParameters.PROJECT_CP_OPT, result, nullValue());
+        assertThat("Missing option " + CommandLineParameters.PROJECT_CP_OPT, result, nullValue());
     }
 
 
