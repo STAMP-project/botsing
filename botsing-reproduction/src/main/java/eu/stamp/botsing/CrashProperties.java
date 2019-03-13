66ae245d743962fc7c85602f6033dbbe70cfc11a
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2019-03-13 17:59:38 +0100

    Add the missing import

--- botsing-reproduction/src/main/java/eu/stamp/botsing/CrashProperties.java
+++ botsing-reproduction/src/main/java/eu/stamp/botsing/CrashProperties.java
@@ -31,7 +31,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
+import java.util.List;
 
 
 public class CrashProperties {
