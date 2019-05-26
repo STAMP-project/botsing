9da7bb89c3fe0ef9027acc7e0ef2a53cba970ba1
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2019-05-26 22:25:28 +0200

    Update previous test case according to the recent changes

--- botsing-reproduction/src/test/java/eu/stamp/botsing/reproduction/CrashReproductionGoalFactoryTest.java
+++ botsing-reproduction/src/test/java/eu/stamp/botsing/reproduction/CrashReproductionGoalFactoryTest.java
@@ -67,8 +67,10 @@ public class CrashReproductionGoalFactoryTest {
     @Test
     public void testCrashReproductionGoalFactory() {
         // ToDo: We should update this test with the acceptance of list of crashes feature
-        assertEquals("class eu.stamp.botsing.fitnessfunction.IntegrationTestingFF", crashReproductionGoalFactory.getCoverageGoals().get(0).getClass().toString());
+//        assertEquals("class eu.stamp.botsing.fitnessfunction.IntegrationTestingFF", crashReproductionGoalFactory.getCoverageGoals().get(0).getClass().toString());
 
+
+        CrashProperties.fitnessFunctions = new CrashProperties.FitnessFunction[]{CrashProperties.FitnessFunction.IntegrationSingleObjective};
         crashReproductionGoalFactory = new CrashReproductionGoalFactory();
         assertEquals("class eu.stamp.botsing.fitnessfunction.IntegrationTestingFF", crashReproductionGoalFactory.getCoverageGoals().get(0).getClass().toString());
 
