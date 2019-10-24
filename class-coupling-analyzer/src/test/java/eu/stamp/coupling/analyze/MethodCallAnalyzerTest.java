04b8f277b03ca4be2d47cf42bf102876a4399638
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2019-10-24 20:49:53 +0200

    Fix a flaky test

--- class-coupling-analyzer/src/test/java/eu/stamp/coupling/analyze/MethodCallAnalyzerTest.java
+++ class-coupling-analyzer/src/test/java/eu/stamp/coupling/analyze/MethodCallAnalyzerTest.java
@@ -35,9 +35,19 @@ public class MethodCallAnalyzerTest {
         List<ClassPair> finalResult = methodCallAnalyzer.getFinalList();
 
         Assert.assertEquals(2, finalResult.size());
-        ClassPair pair = finalResult.get(1);
-        Assert.assertEquals(2, pair.getTotalScore());
-        Assert.assertEquals(6, pair.getNumberOfBranchesInClass1()+pair.getNumberOfBranchesInClass2());
+
+        ClassPair pair1 = finalResult.get(0);
+        ClassPair pair2 = finalResult.get(1);
+        if(pair1.getClass1()== "eu.stamp.botsing.coupling.Caller" || pair1.getClass2() == "eu.stamp.botsing.coupling.Caller"){
+            Assert.assertEquals(6, pair1.getNumberOfBranchesInClass1()+pair1.getNumberOfBranchesInClass2());
+            Assert.assertEquals(2, pair2.getNumberOfBranchesInClass1()+pair2.getNumberOfBranchesInClass2());
+        }else {
+            Assert.assertEquals(2, pair1.getNumberOfBranchesInClass1()+pair1.getNumberOfBranchesInClass2());
+            Assert.assertEquals(6, pair2.getNumberOfBranchesInClass1()+pair2.getNumberOfBranchesInClass2());
+        }
+        Assert.assertEquals(2, pair1.getTotalScore());
+        Assert.assertEquals(2, pair2.getTotalScore());
+
     }
 
     @Test
@@ -53,9 +63,18 @@ public class MethodCallAnalyzerTest {
         methodCallAnalyzer.execute("eu.stamp.botsing.coupling.Callee");
         List<ClassPair> finalResult = methodCallAnalyzer.getFinalList();
         Assert.assertEquals(2, finalResult.size());
-        ClassPair pair = finalResult.get(1);
-        Assert.assertEquals(2, pair.getTotalScore());
-        Assert.assertEquals(6, pair.getNumberOfBranchesInClass1()+pair.getNumberOfBranchesInClass2());
+
+        ClassPair pair1 = finalResult.get(0);
+        ClassPair pair2 = finalResult.get(1);
+        if(pair1.getClass1()== "eu.stamp.botsing.coupling.Caller" || pair1.getClass2() == "eu.stamp.botsing.coupling.Caller"){
+            Assert.assertEquals(6, pair1.getNumberOfBranchesInClass1()+pair1.getNumberOfBranchesInClass2());
+            Assert.assertEquals(2, pair2.getNumberOfBranchesInClass1()+pair2.getNumberOfBranchesInClass2());
+        }else {
+            Assert.assertEquals(2, pair1.getNumberOfBranchesInClass1()+pair1.getNumberOfBranchesInClass2());
+            Assert.assertEquals(6, pair2.getNumberOfBranchesInClass1()+pair2.getNumberOfBranchesInClass2());
+        }
+        Assert.assertEquals(2, pair1.getTotalScore());
+        Assert.assertEquals(2, pair2.getTotalScore());
 
 
     }
