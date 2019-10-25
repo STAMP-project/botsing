c3cd072c290561366ed1018511841d36ae84634f
pderakhshanfar <p.derakhshanfar@tudelft.nl>
2019-10-25 15:19:23 +0200

    Add tests for classes in 3 packages of botsing-commons (analysis, coverage, and fitnessfunction)

---
+++ botsing-commons/src/test/java/eu/stamp/botsing/commons/analysis/classpath/CPAnalyzerTest.java
@@ -0,0 +1,42 @@
+package eu.stamp.botsing.commons.analysis.classpath;
+
+import org.evosuite.classpath.ClassPathHacker;
+import org.evosuite.classpath.ClassPathHandler;
+import org.evosuite.setup.InheritanceTree;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CPAnalyzerTest {
+
+    List<String> cps = new ArrayList();;
+
+    @Before
+    public void before(){
+        // setup of the directory with file *class and file *.log
+        String user_dir = System.getProperty("user.dir"); // the current directory is the module
+        // <b>botsing-reproduction</b>
+        File file = new File(user_dir);
+        String base_dir = Paths.get(file.getParent(), "botsing-examples").toString(); // the crash to replicate is
+        // Prepare projectCP
+        String projectCP = Paths.get(base_dir, "target", "classes").toString();
+        cps.add(projectCP);
+    }
+
+    @Test
+    public void testInheritanceTreeGeneration(){
+        ClassPathHandler.getInstance().changeTargetClassPath(cps.toArray(new String[cps.size()]));
+        CPAnalyzer.analyzeClass(cps);
+        InheritanceTree inheritanceTree = CPAnalyzer.getInheritanceTree();
+        assert(inheritanceTree.hasClass("eu.stamp.botsing.Fraction"));
+        assert(inheritanceTree.hasClass("eu.stamp.botsing.poly.SuperClass"));
+        assert(inheritanceTree.hasClass("eu.stamp.botsing.coupling.Caller"));
+        assert(inheritanceTree.getSuperclasses("eu.stamp.botsing.poly.SubClass").contains("eu.stamp.botsing.poly.SuperClass"));
+    }
+}
---
+++ botsing-commons/src/test/java/eu/stamp/botsing/commons/coverage/branch/IntegrationTestingBranchCoverageGoalTest.java
@@ -0,0 +1,38 @@
+package eu.stamp.botsing.commons.coverage.branch;
+
+import eu.stamp.botsing.commons.BotsingTestGenerationContext;
+import org.evosuite.coverage.ControlFlowDistance;
+import org.evosuite.graphs.cfg.BytecodeInstruction;
+import org.evosuite.graphs.cfg.BytecodeInstructionPool;
+import org.evosuite.testcase.execution.ExecutionResult;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class IntegrationTestingBranchCoverageGoalTest {
+
+    @Test
+    public void test(){
+        String className = "org.example.Class";
+        String methodName = "method0";
+        int lineNumber = 1;
+
+        BytecodeInstruction instruction = Mockito.mock(BytecodeInstruction.class);
+        Mockito.doReturn(className).when(instruction).getClassName();
+        Mockito.doReturn(methodName).when(instruction).getMethodName();
+        Mockito.doReturn(lineNumber).when(instruction).getLineNumber();
+
+
+        BytecodeInstructionPool pool = BytecodeInstructionPool.getInstance(BotsingTestGenerationContext.getInstance().getClassLoaderForSUT());
+        pool.registerInstruction(instruction);
+
+        IntegrationTestingBranchCoverageGoal coverageGoal = new IntegrationTestingBranchCoverageGoal(className,methodName);
+
+        ExecutionResult result = Mockito.mock(ExecutionResult.class, Mockito.RETURNS_DEEP_STUBS);
+        Mockito.when(result.getTrace().getCoveredMethods().contains(className + "." + methodName)).thenReturn(true);
+
+        ControlFlowDistance resultDistance = coverageGoal.getDistance(result);
+        Assert.assertEquals(0, resultDistance.getApproachLevel());
+        Assert.assertEquals(0, resultDistance.getBranchDistance(),0);
+    }
+}
