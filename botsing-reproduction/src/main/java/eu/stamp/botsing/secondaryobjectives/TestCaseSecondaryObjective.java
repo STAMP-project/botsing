663a5b521b1956d940113b5d53e69dab10074078
pderakhshanfar <pouria.derakhshanfar@gmail.com>
2020-04-22 13:42:29 +0200

    Log Secondary Objectives

--- botsing-reproduction/src/main/java/eu/stamp/botsing/secondaryobjectives/TestCaseSecondaryObjective.java
+++ botsing-reproduction/src/main/java/eu/stamp/botsing/secondaryobjectives/TestCaseSecondaryObjective.java
@@ -1,13 +1,17 @@
 package eu.stamp.botsing.secondaryobjectives;
 
 import eu.stamp.botsing.CrashProperties;
+import eu.stamp.botsing.ga.strategy.GuidedGeneticAlgorithm;
 import org.evosuite.testcase.TestChromosome;
 import org.evosuite.testcase.secondaryobjectives.MinimizeLengthSecondaryObjective;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TestCaseSecondaryObjective {
-
+    private static final Logger LOG = LoggerFactory.getLogger(TestCaseSecondaryObjective.class);
 
     public static void setSecondaryObjectives() {
+        LOG.info("Secondary Objectives: {}",CrashProperties.secondaryObjectives);
         for (CrashProperties.SecondaryObjective secondaryObjective : CrashProperties.secondaryObjectives) {
             switch (secondaryObjective) {
                 case BasicBlockCoverage:
