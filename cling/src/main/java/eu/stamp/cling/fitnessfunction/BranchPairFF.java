a0bb06493c1ac2ae2c5bac27f92fb6123cba8547
pderakhshanfar <pouria.derakhshanfar@gmail.com>
2021-04-07 12:02:51 +0200

    Fix the Checkstyle violation

--- cling/src/main/java/eu/stamp/cling/fitnessfunction/BranchPairFF.java
+++ cling/src/main/java/eu/stamp/cling/fitnessfunction/BranchPairFF.java
@@ -15,7 +15,6 @@ import org.evosuite.testcase.execution.ExecutionTrace;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class BranchPairFF extends TestFitnessFunction {
